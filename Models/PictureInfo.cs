8b452ae11ea92f2a102861ff9fb3b46c2c6ecc00
unknown <5199706636@qq.com>
2020-10-08 22:51:56 +0800

    init

---
+++ Models/PictureInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureSort.Models
+{
+    public class PictureInfo
+    {
+        public int Id { get; set; }
+        public bool IsCatched { get; set; } = false;
+        public string Remark { get; set; }
+        public string SubFolder { get; set; }
+    }
+}
---
+++ Repositories/PsRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PictureSort.Models;
+using Npoi.Mapper;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace PictureSort.Repositories
+{
+    public class PsRepository
+    {
+        public List<PictureInfo> ImportSource(string path)
+        {
+            var mapper = new Mapper(path);
+            var pInfos = mapper.Take<PictureInfo>().Select(x=>x.Value).ToList();
+            return pInfos;
+        }
+
+        public Task Sort(List<PictureInfo> pInfos, string target)
+    }
+}
